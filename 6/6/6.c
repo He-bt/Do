3fd5505d162f389f1ec8a2661b05d3297424f4cf
HE258144 <2581449400@QQ.COM>
2019-02-23 00:29:19 +0800

    6

---
+++ 6/6/6.c
@@ -0,0 +1,62 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+//int print(int num)
+//{
+//	int i = 0;
+//	for (i = 1; i <= num; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d", j*i);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print(n);
+//	system("pause");
+//	return 0;
+//}
+//
+//int swap(int *a, int *b)
+//{
+//	int temp;
+//	temp = *a;
+//	*a = *b;
+//	*b = temp;
+//}
+//main()
+//{
+//	int x, y;
+//	printf("请输入x与y的值，用空格隔开\n");
+//	scanf("%d %d", &x, &y);
+//	printf("x=%d,y=%d\n", x, y);
+//	swap(&x, &y);
+//	printf("交换后\nx=%d,y=%d\n", x, y);
+//}
+//
+//ntfun(int n);
+//int fun(int n)
+//{
+//	int i;
+//
+//	for (i = 2; i<n; i++)//for(i=2;i<=sqrt(n);i++)
+//	if (n%i == 0)
+//		return 0;
+//	return 1;
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	if (fun*(n))
+//		printf("Yes!\n");
+//	return 0;
+//}
